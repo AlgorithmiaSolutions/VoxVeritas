7c0d4a11e9062d19cfa85dbec50e463375e74eb2
Ron Ismaili <125833337+RonIsmaili@users.noreply.github.com>
2024-05-26 08:51:08 +0200

    ADD: initial flask back-end

--- Flask/app.py
+++ Flask/app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'mknafihia;nfiuqwnpf982no2iqn'
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 
 @app.route('/', methods=['GET'])
-def index_get():
-    return render_template('index.html', message='Hello World!')
+@app.route('/<message>', methods=['GET'])
+def index_get(message=None):
+    return render_template('index.html', message=message)
 
 
 @app.route('/submit', methods=['POST'])
 def submit_audio():
-    return render_template('index.html', message='Huuuu')
+    if 'ai_audio' not in request.files:
+        return redirect(url_for('index_get', message='No file part!'))
+
+    file = request.files['ai_audio']
+
+    if file.filename == '':
+        return redirect(url_for('index_get', message='No selected file!'))
+
+    if file:
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return redirect(url_for('index_get', message='File uploaded successfully!'))
+    return redirect(url_for('index_get', message='File failed to upload!'))
 
 
 if __name__ == '__main__':
